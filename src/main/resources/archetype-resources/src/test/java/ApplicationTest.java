772ae0979add0586027c57fbaf14f91544df2a20
Mladen Čikara <mladen.cikara@gmail.com>
2018-07-25 19:48:57 +0200

    initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -0,0 +1,18 @@
+package ${package};
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ApplicationTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+}
